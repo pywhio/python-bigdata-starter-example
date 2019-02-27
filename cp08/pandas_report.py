12f9199ce33f2671f46517a616024baf961421dc
zengqingbo <zengqingbo@139.com>
2019-02-27 11:48:43 +0800

    alter cp08

--- cp08/pandas_report.py
+++ cp08/pandas_report.py
@@ -1,24 +1,39 @@
 import pandas as pd
-from reports_cfg import re_cfg
+from reports_cfg import re_cfg, add_timefield
 import sys,os
 
 sys.path.append('../')
 from cp03.merge_csv  import get_FileSize
 
-def report(filename,re_cfg=re_cfg):
+def report(filename, add_fields=None, re_cfg=re_cfg):
     result = {}
     df = pd.read_csv(filename)
     print('loaded csv file: %s' % filename)
+
+    #根据新增字段配置，增加字段
+    df = df.apply(add_timefield, axis='columns') if add_fields else df
+
     for report_name, agg_cfg in re_cfg.items():
         csv_name = os.path.splitext(filename)[0]+'-'+report_name+'.csv'
-        df.groupby(agg_cfg['groupby_fields']).agg(agg_cfg['agg_fields']).to_csv(csv_name)
+
+        #根据配置执行grouby和aggegration操作
+        df_agg = df.groupby(agg_cfg['groupby_fields']).agg(agg_cfg['agg_fields'])
+        #根据是否2层columns,看是降为1层
+        if df_agg.columns.nlevels ==2 :
+            df_agg.columns = ['_'.join(c) for c in df_agg.columns]
+
+        #根据配置重命名
+        df_agg.rename(agg_cfg['map_fields'], axis='columns')
+
+        #输出csv
+        df_agg.to_csv(csv_name)
         print('generated report file: %s' % csv_name)
         result[csv_name] = [True,get_FileSize(csv_name)] if os.path.isfile(csv_name) else [False,None]
     print('return result: %s' % result)
     return result
 
 if __name__ == '__main__':
-    report('QLR201902071000-merged.csv')
+    report('QLR201902071000-merged.csv', add_fields=add_timefield)
 
 
 # ➜  cp08 git:(master) ✗ python pandas_report.py
--- cp08/reports_cfg.py
+++ cp08/reports_cfg.py
@@ -1,10 +1,23 @@
 import numpy as np
 
+
+def add_timefield(s):
+    """recieve a row Series ,transform it, then return the Series"""
+    s['timescale'] = str(s['交易时间'])[:-2]
+    return s
+
 re_cfg = {
-    're-site':{'groupby_fields':['交易网点','交易类型'],
-                'agg_fields':{'交易金额':'sum'},
+    ## pandas has 'mean', 'median', 'prod', 'sum', 'std', 'var', 'count'
+    're-site':{
+                'groupby_fields':['timescale','交易网点','交易类型'],  #可对add_fields中定义的字段进行groupby操作
+                'agg_fields':{'交易金额':['sum','count']}, #可对单个字段增加不同运算方法
+                'map_fields':{} #对最终输出字段重命名
               },
-    're-customer':{'groupby_fields':['客户id','交易类型'],
-                'agg_fields':{'交易金额':np.sum},
+
+    ## numpy has np.sum , np.mean , np.std, np.var .... and user define func
+    're-customer':{
+                'groupby_fields':['timescale','客户id','交易类型'],
+                'agg_fields':{'交易金额':[np.sum,'count']},
+                'map_fields':{}
               },
     }
