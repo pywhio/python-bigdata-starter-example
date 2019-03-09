80617e320039d07087c4a60f767d4e67508a4ca5
zengqingbo <zengqingbo@139.com>
2019-03-09 13:23:17 +0800

    fix multiprocessing bug

--- cp11/ftp_mp_con.py
+++ cp11/ftp_mp_con.py
@@ -55,8 +55,8 @@ def mp_getter(ftps_cfg=conn_info, buffer_path = './buffer_path'):
         filelist = ftp.listdir(ftp_cfg['path'])
         print(f'{Fore.RED}%s filelist: %s{Style.RESET_ALL}' % (ftp_cfg['name'], filelist))
         return mt_getter(ftp_cfg, filelist, buffer_path)
-    with parallel_backend('threading', n_jobs=2):
-        # 使用多线程，2个并发启动下载任务
+    with parallel_backend('multiprocessing', n_jobs=5):
+        # 使用多进程，5个并发启动ftp处理任务
         status = Parallel()(delayed(ftp_getter)(ftp_cfg) for ftp_cfg in ftps_cfg.values() )
     result = dict(zip(ftps_cfg.keys(), status))
     print(f'{Fore.RED}result: %s{Style.RESET_ALL}' % result)
